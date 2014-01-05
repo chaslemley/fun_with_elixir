d1815330c7012601f41ac6647c74263fdbdd1ac1
Chas Lemley <chas.lemley@gmail.com>
2014-01-04 22:05:58 -0500

    count cats

--- programming_elixir/13_WorkingWithMultipleProcesses-9.exs
+++ programming_elixir/13_WorkingWithMultipleProcesses-9.exs
@@ -2,27 +2,27 @@ defmodule CatCounter do
   def counter(scheduler) do
     scheduler <- { :ready, self }
     receive do
-      { :counter, folder, client } ->
-        client <- { :answer, folder, cat_counter(folder), self }
+      { :counter, { folder, search_term }, client } ->
+        client <- { :answer, folder, cat_counter(folder, search_term), self }
         counter(scheduler)
       { :shutdown } ->
         exit(0)
     end
   end
 
-  defp cat_counter(folder) do
+  defp cat_counter(folder, search_term) do
     File.ls!(folder)
     |> Enum.map(&("#{folder}/#{&1}"))
-    |> Enum.map(&file_counter/1)
+    |> Enum.map(fn(file) -> file_counter(file, search_term) end)
     |> Enum.reduce &(&1 + &2)
   end
 
-  defp file_counter(filename) do
+  defp file_counter(filename, search_term) do
     parse = File.read!(filename) |> String.to_char_list
     case parse do
       {:ok, char_list} ->
-        Enum.chunk(char_list, 3, 1)
-        |> Enum.count &(&1 == 'cat')
+        Enum.chunk(char_list, String.length(search_term), 1)
+        |> Enum.count &(&1 == String.to_char_list!(search_term))
       _ -> 0
     end
   end
@@ -53,12 +53,11 @@ defmodule Scheduler do
 
       {:answer, number, result, _pid} ->
         schedule_processes(processes, queue, [ {number, result} | results ])
-
     end
   end
 end
 
-to_process = [ "/Users/chas/Projects/elixir/programming_elixir" ]
+to_process = [ { "/Users/chas/Projects/elixir/programming_elixir", "cat" } ]
 
 Enum.each 1..10, fn num_processes ->
   {time, result} = :timer.tc(Scheduler, :run,
